fad65c2c36f45acc0240f39138eac4e5cc524efa
areebcodes <makhan9075@gmail.com>
2026-05-26 22:27:44 +0500

    Embed env vars at build time via next.config.js env block

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    S3_BUCKET: process.env.S3_BUCKET || "image-processor-manual-755289",
+    SQS_QUEUE_URL:
+      process.env.SQS_QUEUE_URL ||
+      "https://sqs.us-east-1.amazonaws.com/755289151223/image-processor-jobs-manual",
+    DYNAMODB_TABLE: process.env.DYNAMODB_TABLE || "image-processor-jobs-manual",
+  },
+};
 module.exports = nextConfig;
